d9274135b22ec59b766ca2f08c5b15799d027b3a
100LAR [Stolar Studio] <nikitstolarov@gmail.com>
2022-12-30 22:26:37 +0300

    кал

--- src/main/java/net/mcreator/createstructures/procedures/Additional_genProcedure.java
+++ src/main/java/net/mcreator/createstructures/procedures/Additional_genProcedure.java
@@ -6,21 +6,22 @@ import net.minecraft.world.level.block.Blocks;
 import net.minecraft.core.BlockPos;
 
 public class Additional_genProcedure {
-	public static boolean execute(LevelAccessor world, double x, double y, double z, int struct_x, int struct_y, int struct_z, int rotate_angle) {
+	public static boolean execute(LevelAccessor world, double x, double y, double z, int struct_x, int struct_y, int struct_z) {
+
+		int max_size = struct_x;
+		if (struct_z > struct_x) {
+			max_size = struct_z;
+		}
+		
 		int size_pos[][] = {
-			{1, 1},
-			{-struct_x + 1, 1},
-			{-struct_x + 1, struct_z},
-			{1, struct_z}	
+			{0, 0},
+			{max_size, 0},
+			{max_size, max_size},
+			{0, max_size}	
 		};
 
-//		System.out.println("----------------------");
 		int i;
-		for (i = 0; i < 4; i++) {
-//			System.out.print(x + size_pos[i][0]);
-//			System.out.print(" ");
-//			System.out.println(y + size_pos[i][1]);
-//			
+		for (i = 0; i < 4; i++) {
 			if ((world.isEmptyBlock(new BlockPos(x + size_pos[i][0], y - 1, z + size_pos[i][1]))) | ((world.getFluidState(new BlockPos(x + size_pos[i][0], y - 1, z + size_pos[i][1])).createLegacyBlock()).getBlock() instanceof LiquidBlock)) {
 				return false;
 			}
--- src/main/java/net/mcreator/createstructures/world/features/VillagehauseFeature.java
+++ src/main/java/net/mcreator/createstructures/world/features/VillagehauseFeature.java
@@ -45,12 +45,29 @@ public class VillagehauseFeature extends Feature<NoneFeatureConfiguration> {
 		return PLACED_FEATURE;
 	}
 
-	public static final Set<ResourceLocation> GENERATE_BIOMES = Set.of(new ResourceLocation("forest"), new ResourceLocation("taiga"),
-			new ResourceLocation("plains"));
+	public static final Set<ResourceLocation> GENERATE_BIOMES = null;//Set.of(new ResourceLocation("forest"), new ResourceLocation("taiga"),
+//			new ResourceLocation("plains"));
 	private final Set<ResourceKey<Level>> generate_dimensions = Set.of(Level.OVERWORLD);
 	private final List<Block> base_blocks;
 	private StructureTemplate template = null;
+	private String[] Resource_list = new String[] {
+		"car",
+		"field_tent",
+		"field_theatre",
+		"grave",
+		"grave2",
+		"grave3",
+		"grave4",
+		"vallage_big_hause",
+		"vallage_hause",
+		"vallage_hause2",
+		"vallage_hause3"
+	};
 
+	private String template_name = "";
+	
+	private boolean spawned = true;
+	
 	public VillagehauseFeature() {
 		super(NoneFeatureConfiguration.CODEC);
 		base_blocks = List.of(Blocks.GRASS_BLOCK);
@@ -60,8 +77,12 @@ public class VillagehauseFeature extends Feature<NoneFeatureConfiguration> {
 	public boolean place(FeaturePlaceContext<NoneFeatureConfiguration> context) {
 		if (!generate_dimensions.contains(context.level().getLevel().dimension()))
 			return false;
-		if (template == null)
-			template = context.level().getLevel().getStructureManager().getOrCreate(new ResourceLocation("create_structures", "vallage_hause"));
+		//if (template == null)
+		if (spawned) {
+			template_name = Resource_list[context.random().nextInt(Resource_list.length)];
+			template = context.level().getLevel().getStructureManager().getOrCreate(new ResourceLocation("create_structures", template_name));
+		}
+		
 		if (template == null)
 			return false;
 
@@ -77,18 +98,39 @@ public class VillagehauseFeature extends Feature<NoneFeatureConfiguration> {
 				int i = context.origin().getX() + context.random().nextInt(16);
 				int k = context.origin().getZ() + context.random().nextInt(16);
 				int j = context.level().getHeight(Heightmap.Types.OCEAN_FLOOR_WG, i, k) - 1;
-				if (!base_blocks.contains(context.level().getBlockState(new BlockPos(i, j, k)).getBlock()))
+				if (!base_blocks.contains(context.level().getBlockState(new BlockPos(i, j, k)).getBlock())) {
+					spawned = false;
 					continue;
+				}
 				BlockPos spawnTo = new BlockPos(i + 0, j + 0, k + 0);
 				WorldGenLevel world = context.level();
 				int x = spawnTo.getX();
 				int y = spawnTo.getY();
 				int z = spawnTo.getZ();
+
+				////////////////////////////////////
 				int rotate_angle = context.random().nextInt(3);
-				if (!Additional_genProcedure.execute(world, x, y, z, struct_x, struct_y, struct_z, rotate_angle))
+				
+				int xOffset = 0;
+        		int zOffset = 0;
+				
+				if (rotate_angle == 1) {
+					xOffset -= struct_x;
+				} else if (rotate_angle == 2) {
+					zOffset -= struct_z;
+					xOffset -= struct_x;
+				} else if (rotate_angle == 3) {
+					zOffset += struct_z;
+					xOffset += struct_x;
+				}
+				////////////////////////////////////
+				
+				if (!Additional_genProcedure.execute(world, x + xOffset, y, z + zOffset, struct_x, struct_y, struct_z)) {
+					spawned = false;
 					continue;
+				}
 				if (template.placeInWorld(context.level(), spawnTo, spawnTo,
-						new StructurePlaceSettings().setMirror(Mirror.values()[context.random().nextInt(2)])
+						new StructurePlaceSettings().setMirror(Mirror.values()[0])
 								.setRotation(Rotation.values()[rotate_angle]).setRandom(context.random())
 								.addProcessor(BlockIgnoreProcessor.AIR).setIgnoreEntities(false),
 						context.random(), 2)) {
@@ -96,6 +138,9 @@ public class VillagehauseFeature extends Feature<NoneFeatureConfiguration> {
 				}
 			}
 		}
+		System.out.print("Spawn ");
+		System.out.println(template_name);
+		spawned = true;
 		return anyPlaced;
 	}
 }
